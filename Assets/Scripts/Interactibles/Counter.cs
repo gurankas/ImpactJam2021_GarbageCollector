729304c1d2efe3945bd1ded5c03a4c2e42a7f822
Gurankas SIngh <gurankas@gmail.com>
2021-03-25 14:39:14 -0700

    Transitioning from box overlap to ray cast due to direction sensitivity while selecting object
    Also, re arranged for a better folder structure

---
+++ Assets/Scripts/Interactibles/Counter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Counter : MonoBehaviour, IInteractable
+{
+    public void ChangeSortingOrder(int sortingOrder)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact(Transform other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopInteraction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private Players _player;
+
     [SerializeField]
     private float _speed = 5.0f;
 
@@ -20,25 +21,24 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Transform _hand;
 
-    private IInteractable _curInteractable;
-
     [SerializeField]
     private GameObject _renderer;
+
+    [SerializeField]
+    private float _raycastLength;
+
+    private IInteractable _curInteractable;
     private Animator _animator;
     private Rigidbody2D _rigidbody;
-    public Vector2 _movementVector;
-
-
     private KeyCode[] _keyCodes = new KeyCode[5];
-
-    IsometricCharacterRenderer isoRenderer;
+    private IsometricCharacterRenderer _isoRenderer;
 
     private void Awake()
     {
         _animator = _renderer.GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody2D>();
 
-        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+        _isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
 
         if (_player == Players.Player1)
         {
@@ -61,20 +61,58 @@ public class Player : MonoBehaviour
     //All input handling should be in Update
     private void Update()
     {
-        //movement
-        if (Input.GetKey(_keyCodes[0]) || Input.GetKey(_keyCodes[1]) || Input.GetKey(_keyCodes[2]) || Input.GetKey(_keyCodes[3]))
-        {
-            Vector2 currentPos = _rigidbody.position;
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-            inputVector = Vector2.ClampMagnitude(inputVector, 1);
-            Vector2 movement = inputVector * _speed;
-            Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-            isoRenderer.SetDirection(movement);
-            _rigidbody.MovePosition(newPos);
-        }
+        Movement();
+        //Interact();
 
+        Vector2 direction = DetermineDirection(_isoRenderer.LastDirection);
+        Debug.DrawRay(_hand.position, direction * _raycastLength, Color.red, .5f);
+        var hitResult = Physics2D.Raycast(_hand.position, direction, _raycastLength, _intaractableMask);
+        Debug.Log($"{hitResult.collider.gameObject.name}");
+    }
+
+    private Vector2 DetermineDirection(int lastDirection)
+    {
+        switch (lastDirection)
+        {
+            case 0:
+                {
+                    return Vector2.up;
+                }
+            case 1:
+                {
+                    return Vector2.up + Vector2.left;
+                }
+            case 2:
+                {
+                    return Vector2.left;
+                }
+            case 3:
+                {
+                    return Vector2.left + Vector2.down;
+                }
+            case 4:
+                {
+                    return Vector2.down;
+                }
+            case 5:
+                {
+                    return Vector2.down + Vector2.right;
+                }
+            case 6:
+                {
+                    return Vector2.right;
+                }
+            case 7:
+                {
+                    return Vector2.right + Vector2.up;
+                }
+            default:
+                return Vector2.up;
+        }
+    }
+
+    private void Interact()
+    {
         //Action (interact)
         if (Input.GetKeyDown(_keyCodes[4]))
         {
@@ -96,6 +134,23 @@ public class Player : MonoBehaviour
                 _curInteractable.StopInteraction();
                 _curInteractable = null;
             }
+        }
+    }
+
+    private void Movement()
+    {
+        //movement
+        if (Input.GetKey(_keyCodes[0]) || Input.GetKey(_keyCodes[1]) || Input.GetKey(_keyCodes[2]) || Input.GetKey(_keyCodes[3]))
+        {
+            Vector2 currentPos = _rigidbody.position;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+            inputVector = Vector2.ClampMagnitude(inputVector, 1);
+            Vector2 movement = inputVector * _speed;
+            Vector2 newPos = currentPos + movement * Time.deltaTime;
+            _isoRenderer.SetDirection(movement);
+            _rigidbody.MovePosition(newPos);
         }
     }
 
