74a0148bae20a2c92d66b304d2dd62467a73c948
Gurankas SIngh <gurankas@gmail.com>
2021-03-25 00:28:26 -0700

    Added the option of having prefab tiles in the tile pallet
    We can add all interactibles in a different grid via the tile map (and even the players too)

--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -4,4 +4,5 @@ public interface IInteractable
 {
     void Interact(Transform other);
     void StopInteraction();
+    void ChangeSortingOrder(int sortingOrder);
 }
--- Assets/Scripts/Pickable.cs
+++ Assets/Scripts/Pickable.cs
@@ -6,12 +6,15 @@ public class Pickable : MonoBehaviour, IInteractable
 
     private Transform _holder;
 
+    private SpriteRenderer _sr;
+
     [SerializeField]
     private float _lerpSpeed = 8f;
 
     public void Interact(Transform attachTransform)
     {
         _holder = attachTransform;
+        _sr = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -24,5 +27,10 @@ public class Pickable : MonoBehaviour, IInteractable
     public void StopInteraction()
     {
         _holder = null;
+    }
+
+    public void ChangeSortingOrder(int sortingOrder)
+    {
+        _sr.sortingOrder = sortingOrder;
     }
 }
