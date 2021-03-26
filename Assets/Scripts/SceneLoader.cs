0314e4a2e5125cb9d8789f4726bfc716239131f9
Gurankas SIngh <gurankas@gmail.com>
2021-03-26 15:43:25 -0700

    UI Button sound integration in the menu

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,20 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("script loaded");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
